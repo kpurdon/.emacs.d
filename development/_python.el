79c3d36c9d72c3e308cc17c7197d694da70f94f0
Kyle Purdon <kylepurdon@gmail.com>
2019-01-10 15:54:19 -0700

    uses PYTHONPATH and enables py-isort

--- development/_python.el
+++ development/_python.el
@@ -10,6 +10,8 @@
 
 (elpy-enable)
 
+(exec-path-from-shell-copy-env "PYTHONPATH")
+
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
@@ -23,6 +25,10 @@
 (require 'py-autopep8)
 (setq py-autopep8-options '("--ignore=E501,W293,W391,W690"))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+
+;; enable isort import sorting
+(require 'py-isort)
+(add-hook 'before-save-hook 'py-isort-before-save)
 
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
